af8835f97d96cd78f85099b42fd58f0d439da4c0
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-07-18 21:07:56 +0100

    DB Structure Changes
    - TweetId + Check
    - Files' Table

--- db/raspi-tv.sql
+++ db/raspi-tv.sql
@@ -4,23 +4,32 @@ DROP TABLE IF EXISTS [Accounts];
 
 DROP TABLE IF EXISTS [Tweets];
 
+DROP TABLE IF EXISTS [Files];
+
 CREATE TABLE [Users](
-  [UserId]    TEXT PRIMARY KEY  NOT NULL,
-  [Password]  TEXT              NOT NULL,
-  [FirstName] TEXT              NOT NULL,
-  [LastName]  TEXT              NOT NULL,
-  [Email]     TEXT              NOT NULL
+  [UserId]    TEXT PRIMARY KEY  NOT NULL,  -- Username
+  [Password]  TEXT              NOT NULL,  -- Password
+  [FirstName] TEXT              NOT NULL,  -- User's first name
+  [LastName]  TEXT              NOT NULL,  -- User's last name
+  [Email]     TEXT              NOT NULL   -- User's email
 );
 
 CREATE TABLE [Accounts](
-  [Id]          TEXT PRIMARY KEY  NOT NULL,
-  [Password]    TEXT              NOT NULL,
-  [ServiceName] TEXT              NOT NULL
+  [AccountId] TEXT PRIMARY KEY  NOT NULL,  -- Account Id to authenticate with
+  [AuthToken] TEXT              NOT NULL,  -- Authentication Token used
+  [Service]   TEXT              NOT NULL   -- Name of the related service (e.g.: 'dropbox', 'twitter', ...)
 );
 
 CREATE TABLE [Tweets](
-  [Author] TEXT NOT NULL,
-  [Tweet]  TEXT NOT NULL
+  [TweetId]   TEXT PRIMARY KEY  NOT NULL,  -- Tweet Id
+  [Author]    TEXT              NOT NULL,  -- Tweet's author
+  [Tweet]     TEXT              NOT NULL,  -- Tweet's text (actual tweet)
+  [ToDisplay] INTEGER           NOT NULL   -- 'Boolean value' determining whether to display the tweet or not
+);
+
+CREATE TABLE [Files](
+  [FilePath]  TEXT PRIMARY KEY  NOT NULL,  -- Absolute path to file
+  [ToDisplay] INTEGER           NOT NULL   -- 'Boolean value' determining whether to use the file or not
 );
 
 INSERT INTO [Users] ([UserId], [Password], [FirstName], [LastName], [Email]) VALUES (
