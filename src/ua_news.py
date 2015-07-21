45cdcef8e101d0fc8fb910016a785296a6709e01
Daniela Simões <danielasimoes@ua.pt>
2015-07-21 14:38:43 +0100

    iso-8859-1 to utf-8 done

--- src/ua_news.py
+++ src/ua_news.py
@@ -1,22 +1,27 @@
-import xmltodict
-import urllib2
 import feedparser
+import urllib
 
 
 def deti_news():
-    # obter atraves do url
     feed_content = feedparser.parse('http://services.web.ua.pt/deti/news/')
 
     news = {"title": feed_content.feed.title, "news": []}
 
     for entry in feed_content.entries:
-        print entry["author"]
-        news["news"] += [{"author": entry.author,
+        news["news"] += [{"author": parse_author(entry.author),
                           "summary": entry.summary,
                           "title": entry.title,
                           "date": str(entry.updated)}]
-
     return news
+
+
+def parse_author(author):
+    if "=?iso-8859-1?Q?" in author:
+        slices = urllib.unquote(author.replace("=", "%").replace("\"", "")).replace("?%", "").split("%?iso-8859-1?Q?")
+        author = ""
+        for slice in slices:
+            author += slice.strip().replace("_", " ")
+    return author
 
 
 if __name__ == '__main__':
