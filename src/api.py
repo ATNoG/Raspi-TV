e6c54c9516dbadfdaa8ad5a86935b94706cdac24
Pedro Martins <pbmartins@ua.pt>
2015-07-25 00:52:58 +0100

    Added Dropbox API authentication. Not complete in api.py.

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ import json
 import os
 from ua_news import deti_news
 from weather import get_weather as get_w
+from dropbox import login as dropbox_login, list_files
 
 
 class Api:
@@ -14,3 +15,17 @@ class Api:
     def get_weather(self):
         return json.dumps({"status": 200, "content": get_w()})
 
+
+    @cherrypy.expose
+    def dropbox_new_account(self, code):
+        client_info = dropbox_login(code)
+        # Add info to database using client info
+
+    @cherrypy.expose
+    def get_dropbox_files(self):
+        # Query to database to get all accounts linked
+        # all_files = []
+        # for account in accounts:
+        #    files = list_files(account_access_token)
+        #    all_files.append('account_id': account_id, 'files': files)
+        #return json.dumps({"status": 200, "content": all_files})
