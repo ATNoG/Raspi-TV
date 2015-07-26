34f40ef8448826864cdd8e670e0fb543c10d90d3
Pedro Martins <pbmartins@ua.pt>
2015-07-26 21:40:40 +0100

    Corrected download Dropbox folder to Dropbox API file.

--- src/dropbox_conn.py
+++ src/dropbox_conn.py
@@ -1,5 +1,7 @@
 import dropbox
 import os
+import sys
+import time
 
 
 def list_files(access_token, path):
@@ -9,13 +11,26 @@ def list_files(access_token, path):
     return folder_files
 
 
+def save_file(access_token, path, f):
+    out = open('dropbox_files/' + access_token + '/' + path, 'wb')
+    out.write(f.read())
+    out.close()
+
+
 def download_file(access_token, path):
     client = dropbox.client.DropboxClient(access_token)
 
     f, metadata = client.get_file_and_metadata(path)
-    out = open('dropbox_files/' + access_token + '/' + path, 'wb')
-    out.write(f.read())
-    out.close()
+    last_mod_date = metadata['modified'][0:3] + metadata['modified'][7:11] + metadata['modified'][4:7] \
+                    + metadata['modified'][16:25] + metadata['modified'][11:16]
+    # Check if it was changed
+    if os.path.isfile('dropbox_files/' + access_token + '/' + path):
+        saved_f = open('dropbox_files/' + access_token + '/' + path, os.O_RDWR|os.O_CREAT)
+        info = os.fstat(saved_f)
+        if not time.asctime(time.localtime(info.st_mtime)) == last_mod_date:
+            save_file(access_token, path, f)
+    else:
+        save_file(access_token, path, f)
 
 
 def download_folder(access_token, path):
