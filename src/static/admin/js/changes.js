eb95de41153cb47584e3dbc836ba0bc66f0f0b55
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-08-11 16:34:49 +0100

    Changes.html|Changes.js - Fixes on popovers

--- src/static/admin/js/changes.js
+++ src/static/admin/js/changes.js
@@ -1,54 +1,60 @@
-//Location Popover
+// Location Popover
 $('#popover_location').popover({
     html: true,
     content: function () {
-        console.log("LEEL");
-        return $("#pop_location-content").html();
+        return $('#pop_location-content').html();
     }
 });
-//Location Descrition Popover
+// Location Description Popover
 $('#popover_location_description').popover({
     html: true,
     content: function () {
-        return $("#pop_location_description-content").html();
+        return $('#pop_location_description-content').html();
     }
 });
-//Background Popover
+// Background Popover
 $('#popover_background').popover({
     html: true,
     content: function () {
-        return $("#pop_background-content").html();
+        return $('#pop_background-content').html();
     }
 });
 
 
 $(document).ready(function () {
 
-    $.get( "/updating/retrieveUpdates", function( data ) {
-            for(var i =0 ;i<data.length;i++){
-                if(data[i].type.localeCompare('text') == 0){
-                    $('#'+data[i].id).html(data[i].content);
-                } else if (data[i].type.localeCompare('image')==0){
-                    $("body").css('background-image', 'url(' +'/static/images'+ data[i].content + ')');
-                } else {
-                    //add , if needed
-                }
+    $.get('/updating/retrieveUpdates', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            if (!data[i].type.localeCompare('text')) {
+                $('#popover_' + data[i].id).html(data[i].content);
+            } else if (data[i].type.localeCompare('image') == 0) {
+                $("body").css('background-image', 'url(' + '/static/images' + data[i].content + ')');
+            } else {
+                //add , if needed
             }
-        });
-    var newLocation= NaN;
-    var newLocationDescription= NaN;
-    var newBackground= NaN;
-
-    $("#location_btn").click(function(){
-        newLocation= $('#new_location').val();
+        }
     });
-    $("#location_description_btn").click(function(){
-        newLocationDescription= $('#new_location_description').val();
-    });
-    $("#background_btn").click(function(){
-        newBackground= $('#new_background').val();
-    });  
 
-    $.post("/updating/updateDB", {'location': newLocation, 'locationDescription': newLocationDescription,'background':newBackground});
+    // I don't think this should be run on document.ready
+
+    var newLocation = NaN;
+    var newLocationDescription = NaN;
+    var newBackground = NaN;
+
+    $('#location_btn').click(function () {
+        newLocation = $('#new_location').val();
+    });
+    $('#location_description_btn').click(function () {
+        newLocationDescription = $('#new_location_description').val();
+    });
+    $('#background_btn').click(function () {
+        newBackground = $('#new_background').val();
+    });
+
+    $.post('/updating/updateDB', {
+        'location': newLocation,
+        'locationDescription': newLocationDescription,
+        'background': newBackground
+    });
 
 });
--- src/updating.py
+++ src/updating.py
@@ -4,33 +4,33 @@ import sqlite3 as sql
 
 conn = sql.connect('../db/raspi-tv.sqlite', check_same_thread=False)
 
-class Updating:
 
+class Updating:
     @cherrypy.expose
     @cherrypy.tools.allow(methods=['GET'])
     def retrieveUpdates(self):
-    	cherrypy.response.headers['Content-Type'] = 'text/json'
-        response=[]
-        
-        background= conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('background',)).fetchone()[1]
-        response.append({'id':'background','type':'image', 'content':background})
+        cherrypy.response.headers['Content-Type'] = 'text/json'
+        response = []
 
-        location= conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('location',)).fetchone()[1]
-        response.append({'id':'location','type':'text', 'content':location})
+        background = conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('background',)).fetchone()[1]
+        response.append({'id': 'background', 'type': 'image', 'content': background})
 
-        locationDescription= conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('locationDescription',)).fetchone()[1]
-        response.append({'id':'locationDescription','type':'text', 'content':locationDescription})
+        location = conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('location',)).fetchone()[1]
+        response.append({'id': 'location', 'type': 'text', 'content': location})
+
+        locationDescription = \
+        conn.execute('SELECT * FROM HTMLSettings WHERE idName=?', ('locationDescription',)).fetchone()[1]
+        response.append({'id': 'location_description', 'type': 'text', 'content': locationDescription})
 
         return json.dumps(response)
 
     @cherrypy.expose
     @cherrypy.tools.allow(methods=['POST'])
-    def updateDB (self, location, locationDescription,background):
+    def updateDB(self, location, locationDescription, background):
         if (location != "NaN"):
-    	    conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ',(location,'location',))
+            conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ', (location, 'location',))
         if (locationDescription != "NaN"):
-            conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ',(locationDescription,'locationDescription',))
+            conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ',
+                         (locationDescription, 'locationDescription',))
         if (background != "NaN"):
-            conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ',(background,'background',))
-
-
+            conn.execute('UPDATE HTMLSettings SET content=? WHERE idName=? ', (background, 'background',))
