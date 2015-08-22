e047be3256be5932bab0e6db167855c93f8b452d
Daniela Simões <danielasimoes@ua.pt>
2015-08-22 16:14:29 +0100

    input disabled

--- src/static/admin/js/addVideos.js
+++ src/static/admin/js/addVideos.js
@@ -3,26 +3,34 @@ $(document).ready(function(){
 
     function remove(btn){
         var fieldID = "#field_input" + btn.id.replace("remove", "");
-        var num = fieldID.split("#field_input");
         console.log(fieldID);
-        var tmp = $(fieldID).val();
+        var num = fieldID.split("#field_input");
+        console.log("NUM");
+        console.log(num);
+        var link = $(fieldID).val();
+        console.log("LINK");
+        console.log(link);
+        if(typeof link === 'undefined'){
+            return;
+        };
         $(fieldID).remove();
         $("#remove" + num[1]).remove();
-        $.post( "/youtube/deleteId", {id :tmp}, function( data ) {
+        $.post( "/youtube/delete_link", {link : link}, function( data ) {
         });
     }
 
     $.getJSON("/api/list_of_ids", function( data ){
             console.log(data);
             for(var i=0; i<data.content.length; i++){
-                 $("#field").after('<div class="input-group" id="field' + next + '"><input id="field_input' + next + '" class="form-control" type="text">' + '<span class="input-group-btn"><button id="remove' + next + '" class="btn btn-danger remove-me" type="button">-</button></span></div>');
+                 $("#field").after('<div class="input-group" id="field' + next + '"><input disabled id="field_input' + next + '" class="form-control" type="text">' + '<span class="input-group-btn"><button id="remove' + next + '" class="btn btn-danger remove-me" type="button">-</button></span></div>');
                  $('#field_input' + next).val(data.content[i]);
+
                  next = next + 1;
             }
 
-        $(".remove-me").click(function(){
-            remove(this);
-        });
+            $(".remove-me").click(function(){
+                remove(this);
+            });
 
     });
 
@@ -31,7 +39,7 @@ $(document).ready(function(){
         var addto = "#field";
         var addRemove = "#field_input" + next;
         var link = $("#field_input").val();
-        var newIn = '<div class="input-group" id="field' + next + '"><input id="field_input' + next + '" class="form-control" type="text">';
+        var newIn = '<div class="input-group" id="field' + next + '"><input disabled id="field_input' + next + '" class="form-control" type="text">';
         var newInput = $(newIn);
 
         var removeBtn = '<span class="input-group-btn"><button id="remove' + next + '" class="btn btn-danger remove-me" >-</button></span></div>';
@@ -42,7 +50,8 @@ $(document).ready(function(){
         $("#field" + next).attr('data-source',$(addto).attr('data-source'));
         next = next + 1;
 
-        $.post( "/youtube/saveId", {id: link}, function( data ) {
+
+        $.post( "/youtube/save_link", {link: link}, function( data ) {
         });
 
         $('#field_input').val('');
--- src/youtube.py
+++ src/youtube.py
@@ -6,20 +6,20 @@ class Youtube:
     
     @cherrypy.expose
     @cherrypy.tools.allow(methods=['POST'])
-    def saveId(self, id):
+    def save_link(self, link):
         db = sql.connect('../db/raspi-tv.sqlite')
-        find_id = db.execute("SELECT * FROM YouTube WHERE VideoId = (?);", (id,)).fetchall()
+        find_id = db.execute("SELECT * FROM YouTube WHERE VideoId = (?);", (link,)).fetchall()
 
         if not find_id:
-            db.execute("INSERT INTO YouTube VALUES (?);", (id,))
+            db.execute("INSERT INTO YouTube VALUES (?);", (link,))
             db.commit()
         db.close()
 
     @cherrypy.expose
     @cherrypy.tools.allow(methods=['POST'])
-    def deleteId(self, id):
+    def delete_link(self, link):
         db = sql.connect('../db/raspi-tv.sqlite')
-        find_id = db.execute("SELECT * FROM YouTube WHERE VideoId = (?);", (id,)).fetchone()
+        find_id = db.execute("SELECT * FROM YouTube WHERE VideoId = (?);", (link,)).fetchone()
 
         db.execute("DELETE FROM YouTube WHERE VideoId = (?);", (find_id[0],))
         db.commit()
