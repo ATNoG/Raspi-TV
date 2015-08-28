530b366ce582f52f9c14c3db80a49917209aa6ef
Daniela Simões <danielasimoes@ua.pt>
2015-08-28 23:59:44 +0100

    Reading feed and dowloaded imgs always from db + update when connected

--- src/ua_news.py
+++ src/ua_news.py
@@ -7,7 +7,7 @@ import wget
 
 
 def deti_news():
-
+    open("static/img/feed_imgs/156.jpg", "r")
     try:
         response = requests.get("https://www.google.pt")
         feed_content = feedparser.parse('http://services.web.ua.pt/deti/news/')
@@ -24,19 +24,29 @@ def deti_news():
                             "date": parse_date(str(entry.updated))}]
 
         for i in range(0, len(news["news"])):
-            db.execute("INSERT INTO News VALUES (?,?,?,?);",
-                    (news["news"][i]["title"], news["news"][i]["date"], news["news"][i]["author"], news["news"][i]["summary"]))
-            db.commit()
-
-        for i in range(0, len(news["news"])):
             tmp = news["news"][i]["summary"]
-            #print tmp
-            download_photo(tmp)
+            new_tmp = download_photo(tmp)
 
+            if new_tmp:
+                db.execute("INSERT INTO News VALUES (?,?,?,?);",
+                        (news["news"][i]["title"], news["news"][i]["date"], news["news"][i]["author"], new_tmp))
+                db.commit()
+            else:
+                db.execute("INSERT INTO News VALUES (?,?,?,?);",
+                        (news["news"][i]["title"], news["news"][i]["date"], news["news"][i]["author"], tmp))
+                db.commit()
+
+        news_db = {"title": "", "news": []}
+        for i in db.execute("SELECT * FROM News;").fetchall():
+            news_db["news"] += [{"author": i[2],
+                            "summary": i[3],
+                            "title": i[0],
+                            "date": i[1]}]
 
         db.close()
 
-        return news
+
+        return news_db
     except Exception:
         news_db = {"title": "", "news": []}
         db = sql.connect('../db/raspi-tv.sqlite')
@@ -73,14 +83,15 @@ def download_photo(tmp):
         url = url.group(0)
         url = url.split("\"")[1]
 
-        print url
-
         if "cid" in url:
-            pass
+            return
         else:
             name = url.split("=")
             name = name[1]
-        filename = wget.download(url, "static/img/feed_imgs/" + name + ".jpg")
+            filename = wget.download(url, "static/img/feed_imgs/" + name + ".jpg")
+            new_path_url = ["img/feed_imgs/" + name + ".jpg", url]
+
+            return tmp.replace(new_path_url[1], new_path_url[0])
 
 if __name__ == '__main__':
     resp = deti_news()
