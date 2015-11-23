082ca6d115812faef8b29e2ba54a021dd927ac74
Pedro Martins <pbmartins@ua.pt>
2015-11-23 16:48:18 +0000

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "Installing dependencies"
 apt-get install python-dev python-pip sqlite3 chromium
 pip install -r Raspi-TV/requirements.txt
 chown -R pi:pi Raspi-TV
+echo "Issue 'sudo raspi-config', select 'Enable Boot to Desktop', and choose 'Desktop Log in as user pi at the GUI'"
 echo "Add the following line to '/etc/xdg/lxsession/LXDE-pi/autostart':"
 echo "/usr/bin/chromium --kiosk --ignore-certificate-errors --disable-restore-session-state \"http://localhost:8080/\""
 echo "Please change directory to Raspi-TV:"
@@ -11,4 +12,3 @@ echo "Then, issue:"
 echo "python setup.py create database"
 echo "python setup.py create user"
 echo "python setup.py create cronjobs"
-
