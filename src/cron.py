a1ec3a3e13a3a0b7646cc156d973e2f98003de15
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-01-24 14:50:41 +0000

    Massive cleanup

--- src/cron.py
+++
@@ -1,6 +0,0 @@
-from twitter_conn import populate_db
-from dropbox_conn import copy_dropbox_folder
-
-if __name__ == '__main__':
-    populate_db()
-    copy_dropbox_folder()
--- src/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-def return_base_dir():
-    return str(BASE_DIR)
