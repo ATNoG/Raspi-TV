45cddabb64bca41e7646862dd38f3034954618f5
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-01-25 11:08:07 +0000

    public index added

--- app/static/public/js/raspi-tv.js
+++ app/static/public/js/raspi-tv.js
@@ -15,5 +15,83 @@ function updateTime() {
 ////////////////////////////////////////////////////////
 
 $(document).ready(function () {
+
     setInterval(updateTime, 999);
+
+    function setContentSize(data) {
+        setTimeout(function () {
+            var width_frame = $("#raspi-frame").width();
+            var height_frame = $("#main-row").height();
+
+            var $content_frame = $("#content_frame");
+            $content_frame.width(width_frame);
+            $content_frame.height(height_frame);
+        }, 5000);
+
+        // then call the changes
+        setTimeout(function () {
+            changes();
+        }, 500);
+    }
+
+
+    // Changes
+    //
+    function changes() {
+        $.get("/api/get/HTMLChanges", function (data) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].type.localeCompare('text') == 0) {
+                    $('#' + data[i].id).html(data[i].content);
+                } else if (data[i].type.localeCompare('image') == 0) {
+                    $("body").css('background-image', 'url(' + '/img' + data[i].content + ')');
+                } else {
+                    //add , if needed
+                }
+            }
+
+            // then call get tweets
+            setTimeout(function () {
+                get_tweets();
+            }, 500);
+        });
+    }
+
+    var data_length = 0;
+    var info = [];
+
+    function get_tweets() {
+        $.getJSON('/api/get/tweets', function (data) {
+            data_length = data.length;
+
+            for (var i = 0; i < data.length; i++) {
+                info.push('<strong style="color:#003399;">' + data[i]['author'] + '</strong>:' + '<span style="color:#003399;">' + data[i]['tweet'] + '</span>');
+            }
+            slider();
+
+            // then load the iframe
+            setTimeout(function () {
+                $("#content_frame").attr("src", "pages/news.html");
+            }, 500);
+        });
+    }
+
+    var counter = 0;
+
+    function slider() {
+        var $tweet = $("#tweet");
+        $tweet.html(info[counter++]);
+        $tweet.fadeIn();
+
+        if (counter == data_length)
+            counter = 0;
+
+        setTimeout(function () {
+            $tweet.fadeOut();
+            slider();
+        }, 10000);
+
+
+    }
+
+    setContentSize();
 });
