f65525312a325efd3dd0ebc0f35fcf9cfdf377ad
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-01-27 20:48:18 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,49 +4,52 @@
 # This file should fully setup Raspi-TV on a new Raspberry.
 # For more information visit https://github.com/ATNoG/Raspi-TV.git
 
-echo "If you haven't done it yet don't forget to run raspi-config and update/upgrade your distribution"
-echo "Continuing in 10 seconds..."
+echo "If you haven't done it yet don't forget to run raspi-config and update/upgrade your distribution."
+echo "If you encounter any problems during this setup please look at 'setup.log'."
+echo -n "Continuing in 10 seconds... "
 sleep 10s
+echo "Done."
 
 # Cloning Raspi-TV repository
-echo "Cloning Raspi-TV repository"
-git clone https://github.com/ATNoG/Raspi-TV.git
+echo -n "Cloning Raspi-TV repository... "
+git clone https://github.com/ATNoG/Raspi-TV.git >> setup.log
+echo "Done."
 
 # Set pi as the owner of Raspi-TV and cd to this directory
 chown -R pi:pi Raspi-TV
 pushd Raspi-TV/
 
 # Installing distribution dependencies
-echo "Installing dependencies (this requires root privileges)"
-apt-get -y install $(cat dependencies.txt | grep -v "^#")
+echo -n "Installing dependencies... "
+apt-get -y install $(cat dependencies.txt | grep -v "^#") >> setup.log
+echo "Done."
 
 # Installing Python dependencies
-echo "Installing Python requirements to user pi"
+echo -n "Installing Python requirements... "
 pip install -r app/requirements.txt
+echo "Done."
 
 # Add reboot job as root
-echo "Adding root cronjobs"
+echo "Adding root cronjobs."
 (crontab -l; echo "@reboot 0 0 * * * /sbin/reboot")| crontab -          # Reboot at midnight
 
 # Change user (don't need root for now)
 sudo -u pi bash << EOF
 
 # Adding cronjobs
-echo "Adding pi cronjobs"
+echo "Adding pi cronjobs."
 (crontab -l; echo "@reboot python $(pwd)/app/app.py &")| crontab -      # At every reboot start the server
-(crontab -l; echo "@reboot $(pwd)/fullscreen.sh")| crontab -            # At every reboot start epiphany (web browser)
+(crontab -l; echo "@reboot $(pwd)/fullscreen.sh &")| crontab -          # At every reboot start epiphany (web browser)
 (crontab -l; echo "* * * * * python $(pwd)/app/cron.py")| crontab -     # Update Tweets + Dropbox files every minute
 
 # Create database + user
-echo "Please create a new database:"
-echo "python setup.py create database"
-echo "And then add a new admin user:"
-echo "python setup.py create user"
+echo "Please change directory to $(pwd)/Raspi-TV and:"
+echo "Create a new database"
+echo "  python setup.py create database"
+echo "Add a new admin user"
+echo "  python setup.py create user"
 
 EOF
 popd
 
-echo "Rebooting in 1 minute"
-echo "If you've got unsaved work either save it or execute (as root):"
-echo "killall shutdown"
-shutdown -r +1
+echo "Reboot after you've done the above, please reboot."
