1d6abec476679808dad60bce4b9bfaa68b72b3f5
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-01-28 10:08:09 +0000

    attempt of bugfix

--- fullscreen.sh
+++ fullscreen.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ~/.config
 sudo -u pi epiphany-browser -a -i --profile ~/.config http://localhost:8080/public/ --display=:0 &
-sleep 15s;
+sleep 30s;
 xte "key F11" -x:0
 xte "key F5" -x:0
