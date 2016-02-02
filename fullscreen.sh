2b4200bf904dfbee3899088ea594f1bdccf82eac
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-02-02 10:10:43 +0000

    Refactor on setup.sh

--- fullscreen.sh
+++ fullscreen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 30s;
+sleep 45s;
 mkdir -p ~/.config
 sudo -u pi epiphany-browser -a -i --profile ~/.config http://localhost:8080/public/ --display=:0 &
-sleep 30s;
+sleep 15s;
 xte "key F11" -x:0
 xte "key F5" -x:0
--- setup.sh
+++ setup.sh
@@ -6,33 +6,34 @@
 
 echo "If you haven't done it yet don't forget to run raspi-config and update/upgrade"
 echo "your distribution."
-echo "If you encounter any problems during this setup please look at 'setup.log'."
 echo -n "Continuing in 10 seconds... "
 sleep 10s
 echo "Done."
 
 # Cloning Raspi-TV repository
 echo -n "Cloning Raspi-TV repository... "
-git clone https://github.com/ATNoG/Raspi-TV.git &> setup.log
+git clone https://github.com/ATNoG/Raspi-TV.git > /dev/null
 echo "Done."
 
 # Set pi as the owner of Raspi-TV and cd to this directory
 chown -R pi:pi Raspi-TV
-pushd Raspi-TV/
+pushd Raspi-TV/ > /dev/null
 
 # Installing distribution dependencies
 echo -n "Installing dependencies... "
-apt-get -y install $(cat dependencies.txt | grep -v "^#") &>> setup.log
+apt-get -y install $(cat dependencies.txt | grep -v "^#") > /dev/null
 echo "Done."
 
 # Installing Python dependencies
 echo -n "Installing Python requirements... "
-pip install -r app/requirements.txt &>> setup.log
+pip install -r app/requirements.txt > /dev/null
 echo "Done."
 
 # Add reboot job as root
 echo "Adding root cronjobs."
 (crontab -l; echo "@reboot 0 0 * * * /sbin/reboot")| crontab -          # Reboot at midnight
+(crontab -l; echo "@reboot 0 8 * * * /sbin/reboot")| crontab -          # Reboot at 8:00
+(crontab -l; echo "@reboot 0 16 * * * /sbin/reboot")| crontab -         # Reboot at 16:00
 
 # Stop the screen from going blank + hide the mouse
 echo "Configuring other settings."
@@ -59,4 +60,5 @@ echo "  python setup.py create database"
 echo "Add a new admin user"
 echo "  python setup.py create user"
 
+# Done. Reboot
 echo "Reboot after you've done the above."
