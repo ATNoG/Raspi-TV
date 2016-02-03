11faededeeef8636ec2cbfec2bf8e558d41ba6ed
Ricardo Jesus <rj.bcjesus@gmail.com>
2016-02-03 16:20:02 +0000

    reajusting times

--- fullscreen.sh
+++ fullscreen.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sleep 45s;
+sleep 40s;
+rm -rf ~/.mozilla/
 mkdir -p ~/.config
 sudo -u pi iceweasel -private http://127.0.0.1:8080/public/ --display=:0 &
 sleep 15s;
 xte "key F11" -x:0
+sleep 5s
 xte "key F5" -x:0
